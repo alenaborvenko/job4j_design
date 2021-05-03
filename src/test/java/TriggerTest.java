c08932cd167b5869539af3600a5d9924a6698f6c
Borvenko Alena <alenaborvenko@gmail.com>
2021-05-03 20:11:48 +0700

    Trigger codecov

---
+++ src/test/java/TriggerTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TriggerTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, new Trigger().someLogic());
+    }
+}
