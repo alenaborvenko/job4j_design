5de0b822172db6386b1622a039c43d4d2c00d1dd
Borvenko Alena <alenaborvenko@gmail.com>
2021-05-11 23:06:54 +0700

    5.1. Именованные аргументы [#527483]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,43 @@
+package ru.job4j.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        if (values.isEmpty()) {
+            throw new IllegalArgumentException("Empty parameters in the system");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        for (String param : args) {
+            if (!param.startsWith("-")) {
+                throw new IllegalArgumentException("Key not starts with -. Usage -key=value");
+            }
+            String[] parse = param.split("=");
+            if (parse.length != 2) {
+                throw new IllegalArgumentException("Value not correct. Usage -key=value");
+            }
+            values.put(parse[0].substring(1), parse[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
