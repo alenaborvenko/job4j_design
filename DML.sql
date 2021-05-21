04dfaa4e3e59e913a077d8add248d34937f19b47
Borvenko Alena <alenaborvenko@gmail.com>
2021-05-21 23:20:25 +0700

    0. Что такое база данных. [#6860]

---
+++ DML.sql
@@ -0,0 +1,6 @@
+insert into users (name_user, surname, age) values ('vasya', 'vasechkin', 20);
+select * from users;
+update users set age = 15;
+select * from users;
+delete from users;
+select * from users;
---
+++ Script.sql
@@ -0,0 +1,5 @@
+create table if not exists users(
+id serial primary key,
+name_user varchar(255),
+surname text,
+age int);
